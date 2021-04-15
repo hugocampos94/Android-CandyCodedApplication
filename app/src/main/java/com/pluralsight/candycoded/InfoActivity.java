8201b1de979aedb9337cdf73530b8088b9a7e4f6
Hugo Campos <hugo.campos.94@outlook.com>
2021-04-14 23:28:38 -0600

    feat: solution for project
    Solution for all the steps

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,16 +1,13 @@
 package com.pluralsight.candycoded;
 
-import android.content.res.Resources;
-import android.content.res.XmlResourceParser;
 import android.net.Uri;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
+import android.content.Intent;
 import com.squareup.picasso.Picasso;
-import java.io.IOException;
-import java.util.ArrayList;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -28,12 +25,21 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
+    public void createMapIntent(View view) {
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(mapIntent.resolveActivity(getPackageManager()) != null)
+        {
+            startActivity(mapIntent);
+        }
+    }
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        Uri uri = Uri.parse("tel:0123456789");
+        phoneIntent.setData(uri);
+        startActivity(phoneIntent);
+    }
 
 }
